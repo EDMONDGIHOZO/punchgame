c0b3de7b45db8b74727cba56d0ae5c30e8ca3033
EDMOND G <gihozoedmond@gmail.com>
2020-02-25 17:05:50 +0200

    starting the punch bag game in vue js

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue ({
+
+    el : '#vue-app',
+    data: {
+        issue: [
+            {title: 'title here', content: 'contents here'},
+            {title: 'title here', content: 'contents here'},
+            {title: 'title here', content: '<i>the contents around </i>'}
+        ]
+
+    },
+    methods: {
+      
+    }
+
+});     
